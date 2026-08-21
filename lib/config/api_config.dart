5d507b963d6d7599be4040f792aaf751b92670a9
dzakirzidane <dzakirzidane@gmail.com>
2026-08-21 17:28:54 +0700

    fixing api config to production

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -4,6 +4,6 @@ class ApiConfig {
   static const String _productionUrl =
       'https://wastraquest-production.up.railway.app';
   static const String _localUrl = 'http://10.188.54.148:8000';
-  static const bool useLocal = true;
+  static const bool useLocal = false;
   static String get baseUrl => useLocal ? _localUrl : _productionUrl;
 }
