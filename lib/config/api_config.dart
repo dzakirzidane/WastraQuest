f4636fc95f556f96d41c6f27def01450f1578cf2
dzakirzidane <dzakirzidane@gmail.com>
2026-08-21 17:20:16 +0700

    complete test

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
 
   static const String _productionUrl =
       'https://wastraquest-production.up.railway.app';
-  static const String _localUrl = 'http://192.168.1.5:8000';
-  static const bool useLocal = false;
+  static const String _localUrl = 'http://10.188.54.148:8000';
+  static const bool useLocal = true;
   static String get baseUrl => useLocal ? _localUrl : _productionUrl;
 }
